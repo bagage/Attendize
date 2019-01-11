99093b112204f5b28c03975c634ef8fb4209480f
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-01-11 11:42:25 +0100

    Allow cancellation of offline registrations (https://github.com/Attendize/Attendize/issues/351)

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -510,7 +510,10 @@ class EventCheckoutController extends Controller
 
             $order = new Order();
             $ticket_order = session()->get('ticket_order_' . $event_id);
-            $request_data = $ticket_order['request_data'][0];
+            // properly cancel offline registrations
+            // cf https://github.com/Attendize/Attendize/issues/351
+            $access_count = count($ticket_order['request_data']);
+            $request_data = $ticket_order['request_data'][$access_count - 1];
             $event = Event::findOrFail($ticket_order['event_id']);
             $attendee_increment = 1;
             $ticket_questions = isset($request_data['ticket_holder_questions']) ? $request_data['ticket_holder_questions'] : [];
